912c7b28d497b46f7b4a0672c0d73f8791fcba5e
PWLIMAVERDE <vorfast@gmail.com>
2021-02-27 14:53:17 -0300

    release 1.1.0

--- lib/carregar_temas_package.dart
+++ lib/carregar_temas_package.dart
@@ -1,5 +1,7 @@
 library carregar_temas_package;
 
 export 'src/presenter/carregar_temas_presenter.dart';
+export 'src/presenter/atualizar_cor_temas_presenter.dart';
 export 'src/usecases/entities/dados_theme.dart';
 export 'src/usecases/entities/resultado_theme.dart';
+export 'src/utilitarios/Parametros.dart';
