2010cd7d4f88643cc7ea9238cf50a00eaedbe182
PWLIMAVERDE <vorfast@gmail.com>
2021-02-27 14:28:15 -0300

    atualizar cor repositorio e testes ok

--- test/src/repositories/atualizar_cor_temas_repository_test.dart
+++ test/src/repositories/atualizar_cor_temas_repository_test.dart
@@ -1,30 +1,28 @@
-import 'package:carregar_temas_package/src/usecases/atualizar_cor_temas_usecase.dart';
+import 'package:carregar_temas_package/src/repositories/atualizar_cor_temas_repository.dart';
 import 'package:carregar_temas_package/src/utilitarios/Parametros.dart';
 import 'package:carregar_temas_package/src/utilitarios/erros_carregar_temas.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:retorno_sucesso_ou_erro_package/retorno_sucesso_ou_erro_package.dart';
 
-class AtualizarCorTemasMock extends Mock
-    implements Repositorio<bool, ParametrosAtualizarCorTemas> {}
+class FairebaseAtualizarCorTemasDatasourceMock extends Mock
+    implements Datasource<bool, ParametrosAtualizarCorTemas> {}
 
 void main() {
+  late Datasource<bool, ParametrosAtualizarCorTemas> datasource;
   late Repositorio<bool, ParametrosAtualizarCorTemas> repositorio;
-  late UseCase<bool, ParametrosAtualizarCorTemas> atualizarCorTemas;
   late TempoExecucao tempo;
 
   setUp(() {
     tempo = TempoExecucao();
-    repositorio = AtualizarCorTemasMock();
-    atualizarCorTemas = AtualizarCorTemasUsecase(repositorio: repositorio);
+    datasource = FairebaseAtualizarCorTemasDatasourceMock();
+    repositorio = AtualizarCorTemasRepositorio(datasource: datasource);
   });
 
-  test('Deve retornar um sucesso com Stream<ResultadoTheme>', () async {
+  test('Deve retornar um sucesso com true', () async {
     tempo.iniciar();
-    when(repositorio)
-        .calls(#call)
-        .thenAnswer((_) => Future.value(SucessoRetorno<bool>(resultado: true)));
-    final result = await atualizarCorTemas(
+    when(datasource).calls(#call).thenAnswer((_) => Future.value(true));
+    final result = await repositorio(
       parametros: ParametrosAtualizarCorTemas(
         cor: {
           "r": 60,
@@ -41,7 +39,7 @@ void main() {
     )}");
     tempo.terminar();
     print(
-        "Tempo de Execução do AtualizarCorTemasUsecase: ${tempo.calcularExecucao()}ms");
+        "Tempo de Execução do AtualizarCorTemasRepositorio: ${tempo.calcularExecucao()}ms");
     expect(result, isA<SucessoRetorno<bool>>());
     expect(
         result.fold(
@@ -51,13 +49,10 @@ void main() {
         equals(true));
   });
 
-  test(
-      'Deve retornar um ErroAtualizarCorTemas Erro ao atualizar a cor do tema Cod.01-1',
-      () async {
+  test('Deve retornar um sucesso com false', () async {
     tempo.iniciar();
-    when(repositorio).calls(#call).thenAnswer(
-        (_) => Future.value(SucessoRetorno<bool>(resultado: false)));
-    final result = await atualizarCorTemas(
+    when(datasource).calls(#call).thenAnswer((_) => Future.value(false));
+    final result = await repositorio(
       parametros: ParametrosAtualizarCorTemas(
         cor: {
           "r": 60,
@@ -74,22 +69,22 @@ void main() {
     )}");
     tempo.terminar();
     print(
-        "Tempo de Execução do AtualizarCorTemasUsecase: ${tempo.calcularExecucao()}ms");
-    expect(result, isA<ErroRetorno<bool>>());
+        "Tempo de Execução do AtualizarCorTemasRepositorio: ${tempo.calcularExecucao()}ms");
+    expect(result, isA<SucessoRetorno<bool>>());
     expect(
         result.fold(
           sucesso: (value) => value.resultado,
           erro: (value) => value.erro,
         ),
-        isA<ErroAtualizarCorTemas>());
+        equals(false));
   });
 
   test(
-      'Deve retornar um ErroAtualizarCorTemas Erro ao atualizar a cor do tema Cod.01-2',
+      'Deve retornar um ErroAtualizarCorTemas com Erro ao atualizar a cor do tema Cod.02-1',
       () async {
     tempo.iniciar();
-    when(repositorio).calls(#call).thenThrow(Exception());
-    final result = await atualizarCorTemas(
+    when(datasource).calls(#call).thenThrow(Exception());
+    final result = await repositorio(
       parametros: ParametrosAtualizarCorTemas(
         cor: {
           "r": 60,
@@ -106,7 +101,7 @@ void main() {
     )}");
     tempo.terminar();
     print(
-        "Tempo de Execução do AtualizarCorTemasUsecase: ${tempo.calcularExecucao()}ms");
+        "Tempo de Execução do AtualizarCorTemasRepositorio: ${tempo.calcularExecucao()}ms");
     expect(result, isA<ErroRetorno<bool>>());
     expect(
         result.fold(
